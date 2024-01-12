68d8a77552c2a903403ec421db1be1af146953b3
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-01-12 20:59:52 +0100

    git init

---
+++ dashboard-customers/src/components/AllCustomersContent/AllCustomersContent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllCustomersContent = () => {
+  return <div>AllCustomersContent</div>;
+};
+
+export default AllCustomersContent;
---
+++ dashboard-customers/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,16 @@
+import AllCustomersContent from "../../components/AllCustomersContent/AllCustomersContent";
+import MainContent from "../../components/MainContent/MainContent";
+import SideBar from "../../components/SideBar/SideBar";
+
+const MainPage = () => {
+  return (
+    <div>
+      <SideBar />
+      <MainContent>
+        <AllCustomersContent />
+      </MainContent>
+    </div>
+  );
+};
+
+export default MainPage;
